2e4682347739a7538ae31ee815ee5ed3ef8cf25b
dmanguie <manguilimotan01@gmail.com>
2026-03-06 00:12:09 +0800

    Enhance Vehicle class with properties and methods

--- VehicleRentalSystem/src/models/Vehicle.java
+++ VehicleRentalSystem/src/models/Vehicle.java
@@ -1,4 +1,28 @@
-package models;
+public abstract class Vehicle {
 
-abstract class Vehicle {
+    String vehicleId;
+    String brand;
+    String model;
+    double pricePerDay;
+    VehicleStatus status;
+
+    Vehicle(String vehicleId, String brand, String model, double pricePerDay, VehicleStatus status) {
+        this.vehicleId = vehicleId;
+        this.brand = brand;
+        this.model = model;
+        this.pricePerDay = pricePerDay;
+        this.status = status;
+    }
+
+    boolean isAvailable() {
+        return status == VehicleStatus.AVAILABLE;
+    }
+
+    void setStatus(VehicleStatus newStatus) {
+        this.status = newStatus;
+    }
+
+    String displayName() {
+        return brand + " " + model + " (" + vehicleId + ")";
+    }
 }
