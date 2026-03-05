b415c88c3bc2014c680026809715556eab561fca
dmanguie <manguilimotan01@gmail.com>
2026-03-06 00:11:33 +0800

    Implement Car class with attributes and constructor
    Extend Car class from Vehicle and add properties.

--- VehicleRentalSystem/src/models/Car.java
+++ VehicleRentalSystem/src/models/Car.java
@@ -1,4 +1,13 @@
-package models;
+public class Car extends Vehicle {
 
-public class Car {
+    int numberOfDoors;
+    boolean airConditioned;
+
+    Car(String vehicleId, String brand, String model, double pricePerDay,
+        VehicleStatus status, int numberOfDoors, boolean airConditioned) {
+
+        super(vehicleId, brand, model, pricePerDay, status);
+        this.numberOfDoors = numberOfDoors;
+        this.airConditioned = airConditioned;
+    }
 }
