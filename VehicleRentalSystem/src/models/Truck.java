15ea37eed79019fd8ac8915a3e3f847df3f95cd8
Alexandra <alexandralouie.mauring@cit.edu>
2026-03-05 18:46:18 +0800

    Truck initialization

--- VehicleRentalSystem/src/models/Truck.java
+++ VehicleRentalSystem/src/models/Truck.java
@@ -1,5 +1,15 @@
 package models;
 
-public class Truck {
-    System.out.print("Hi Loveu Devin");
+public class Truck extends Vehicle {
+
+    double loadCapacity;
+    String truckType;
+
+    Truck(String vehicleId, String brand, String model, double pricePerDay,
+          VehicleStatus status, double loadCapacity, String truckType) {
+
+        super(vehicleId, brand, model, pricePerDay, status);
+        this.loadCapacity = loadCapacity;
+        this.truckType = truckType;
+    }
 }
