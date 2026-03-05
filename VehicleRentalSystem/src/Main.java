80ea886ff2809bbe05149ab419a6140a33740a92
Alexandra <alexandralouie.mauring@cit.edu>
2026-03-05 18:31:06 +0800

    Testing Github

--- VehicleRentalSystem/src/Main.java
+++ VehicleRentalSystem/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Vehicle Rental System!");
+        System.out.println("Testing Github by Luwi!");
     }
 }
