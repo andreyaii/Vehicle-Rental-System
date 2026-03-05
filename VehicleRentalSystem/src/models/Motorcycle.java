375026fdb006fc2e718847b7b80f303ac2cdd4b5
Alexandra <alexandralouie.mauring@cit.edu>
2026-03-05 18:51:58 +0800

    Reservation

--- VehicleRentalSystem/src/models/Motorcycle.java
+++ VehicleRentalSystem/src/models/Motorcycle.java
@@ -1,4 +1,15 @@
 package models;
 
-public class Motorcycle {
+public class Motorcycle extends Vehicle {
+
+    int engineCC;
+    boolean helmetIncluded;
+
+    Motorcycle(String vehicleId, String brand, String model, double pricePerDay,
+               VehicleStatus status, int engineCC, boolean helmetIncluded) {
+
+        super(vehicleId, brand, model, pricePerDay, status);
+        this.engineCC = engineCC;
+        this.helmetIncluded = helmetIncluded;
+    }
 }
--- VehicleRentalSystem/src/models/Reservation.java
+++ VehicleRentalSystem/src/models/Reservation.java
@@ -1,4 +1,18 @@
 package models;
 
 public class Reservation {
+
+    String reservationDate;
+    Vehicle vehicle;
+    Customer customer;
+
+    Reservation(String reservationDate, Vehicle vehicle, Customer customer) {
+        this.reservationDate = reservationDate;
+        this.vehicle = vehicle;
+        this.customer = customer;
+    }
+
+    RentalTransaction toRentalTransaction(RentalPeriod rentalPeriod, Payment payment, Insurance insurance) {
+        return new RentalTransaction(customer, vehicle, payment, insurance, rentalPeriod);
+    }
 }
